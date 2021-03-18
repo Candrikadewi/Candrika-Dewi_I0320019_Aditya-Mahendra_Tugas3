9c0a30452f8a57f18638df5f2fecbd56517f7d81
Candrika Dewi <candrikaa18@gmail.com>
2021-03-18 23:54:59 +0700

    Pengerjaan Tugas dan Exercise

--- I0320019_Soal1_Tugas3.py
+++ I0320019_Soal1_Tugas3.py
@@ -1,10 +1,10 @@
-# Menampilkan list 10 teman
+# Menampilkan list nama 10 teman
 list = ['Candrika','Aratia','Angela','Gea','Divana','Hana','Funny','Ayu','Adrian','Afiq']
 
 # Menampilkan isi list indeks nomor 4,6,7
 print("List indeks nomor 4,6, dan 7 yaitu", list[4],",", list[6], ",", "dan", list[7])
 
-# Mengganti nama teman di list 3,5, dan 9
+# Mengganti nama teman pada list 3,5, dan 9
 list[3] = 'Audrey'
 list[5] = 'Bonang'
 list[6] = 'Attar'
@@ -14,7 +14,10 @@ list.append('Bagus')
 list.append('Evelyn')
 
 # Menampilkan semua teman dengan pengulangan
-print(list*2)
+rose = 0
+for white in range (0,12) :
+    print (list[rose])
+    rose+=1
 
 # Menampilkan panjang list
-print(len(list))
+print("Panjang indeks list adalah", len(list))
